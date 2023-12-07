425f8b62d74e62658ac4a9f59467cc0ff5cf8b40
hyunjium <umhj0807@khu.ac.kr>
2023-12-08 03:38:50 +0900

    [FEAT]: ID/비밀번호 조건 바꾸기

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -11,6 +11,7 @@ import org.sopt.dosopttemplate.ServicePool.authServiceSignup
 import org.sopt.dosopttemplate.databinding.ActivitySignupBinding
 import retrofit2.Call
 import retrofit2.Response
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
@@ -28,6 +29,10 @@ class SignUpActivity : AppCompatActivity() {
             val password = binding.etSignupPwText.text.toString()
             val nickname = binding.etSignupNicknameText.text.toString()
             val home = binding.etSignupHomeText.text.toString()
+            val idPattern = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{6,10}$"
+            val pwPattern = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{6,12}$"
+            val patternID = Pattern.compile(idPattern)
+            val patternPW = Pattern.compile(pwPattern)
             val builder = AlertDialog.Builder(this)
             authServiceSignup.signup(RequestSignUpDto(id, password, nickname))
                 .enqueue(object : retrofit2.Callback<Unit> {
@@ -43,29 +48,32 @@ class SignUpActivity : AppCompatActivity() {
                                 binding.etSignupNicknameText.text?.filter { !it.isWhitespace() }
                             val fixHome =
                                 binding.etSignupHomeText.text?.filter { !it.isWhitespace() }
-                            if (binding.etSignupIdText.length() < 6 || binding.etSignupIdText.length() > 10) {
-                                builder.setTitle("ID 조건").setMessage("ID는 6~10 글자만 가능합니다.")
+
+                            val matcher = patternID.matcher(id)
+                            val matcher2 = patternPW.matcher(password)
+                            if (!matcher.find()) {
+                                builder.setTitle("ID 조건").setMessage("ID:영문,숫자 포함 6~10 글자")
                                 binding.etSignupId.error = getString(R.string._6_10)
                                 binding.etSignupPw.error = null
                                 binding.etSignupNickname.error = null
                                 binding.etSignupHome.error = null
                                 builder.show()
-                            } else if (binding.etSignupPwText.length() < 8 || binding.etSignupPwText.length() > 12) {
-                                builder.setTitle("비밀번호 조건").setMessage("비밀번호는 8~12 글자만 가능합니다.")
+                            } else if (!matcher2.find()) {
+                                builder.setTitle("비밀번호 조건").setMessage("비밀번호:영문,숫자,특수문자 포함 6~12 글자")
                                 binding.etSignupId.error = null
                                 binding.etSignupPw.error = getString(R.string._8_12)
                                 binding.etSignupNickname.error = null
                                 binding.etSignupHome.error = null
                                 builder.show()
                             } else if (fixNickname?.isEmpty() == true) {
-                                builder.setTitle("닉네임 조건").setMessage("닉네임은 한 글자 이상만 가능합니다.")
+                                builder.setTitle("닉네임 조건").setMessage("닉네임:한 글자 이상")
                                 binding.etSignupId.error = null
                                 binding.etSignupPw.error = null
                                 binding.etSignupNickname.error = getString(R.string._1_)
                                 binding.etSignupHome.error = null
                                 builder.show()
                             } else if (fixHome?.isEmpty() == true) {
-                                builder.setTitle("거주지 조건").setMessage("거주지는 한 글자 이상만 가능합니다.")
+                                builder.setTitle("거주지 조건").setMessage("거주지:한 글자 이상")
                                 binding.etSignupId.error = null
                                 binding.etSignupPw.error = null
                                 binding.etSignupNickname.error = null
